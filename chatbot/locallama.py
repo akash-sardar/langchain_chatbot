99381e98d34c380882a83635d146655a8bec8e32
Akash-Sardar <lordakashjoy@gmail.com>
2024-06-18 00:41:40 -0400

    client added

--- chatbot/locallama.py
+++ chatbot/locallama.py
@@ -1,5 +1,5 @@
 # Langchain Libraries
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.llms import Ollama
